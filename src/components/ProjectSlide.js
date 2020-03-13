9c148c32e54f8f776e64b1d123126541bd5404be
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2020-03-13 12:17:37 +0100

    convert projectSlide component to use hooks

--- src/components/ProjectSlide.js
+++ src/components/ProjectSlide.js
@@ -1,35 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SlideContainer } from './styled-components/containers';
 import { PreviewImages } from './styled-components/styledParts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-class ProjectSlide extends React.Component {
+const ProjectSlide = props => {
 
-  state = {
-    webView: true,
-  }
+  const [isWebView, setWebView] = useState(true);
 
-  hanleWebClick = (e) => {
-    this.setState({
-      webView: true
-    });
-    const imagesContainer = document.querySelectorAll('.images')[this.props.theIndex]
+  const hanleWebClick = (e) => {
+    setWebView(true);
+    const imagesContainer = document.querySelectorAll('.images')[props.theIndex]
     imagesContainer.classList.remove('mobile-view');
     imagesContainer.classList.add('web-view')
   }
 
-  hanleMobileClick = (e) => {
-    this.setState({
-      webView: false
-    })
-    const imagesContainer = document.querySelectorAll('.images')[this.props.theIndex]
+  const hanleMobileClick = (e) => {
+    setWebView(false);
+    const imagesContainer = document.querySelectorAll('.images')[props.theIndex]
     imagesContainer.classList.add('mobile-view');
     imagesContainer.classList.remove('web-view');
   }
 
-  handleDrag = (e) => {
+  const handleDrag = (e) => {
     // e.preventDefault();
     if (document.innerWidth > 700) return;
     e.persist();
@@ -37,39 +31,37 @@ class ProjectSlide extends React.Component {
     p.scrollTop = p.scrollTop === 40 ? 0 : 40;
   }
 
-  render() {
-    const { project, styleClass, theIndex } = this.props;
-    return (
-      <SlideContainer isActive={styleClass} indice={theIndex} >
-        <h2 className="title">{project.title}</h2>
-        <PreviewImages>
-          <div className="images">
-            <div>
-              {project.previews.map((pic) => <img src={pic} key={pic} />)}
-            </div>
+  const { project, styleClass, theIndex } = props;
+  return (
+    <SlideContainer isActive={styleClass} indice={theIndex} >
+      <h2 className="title">{project.title}</h2>
+      <PreviewImages>
+        <div className="images">
+          <div>
+            {project.previews.map((pic) => <img src={pic} key={pic} />)}
           </div>
-          {project.previews.length > 1 ? <div className="anim-btn">
-            <span onClick={this.hanleWebClick} className={this.state.webView ? 'active' : null}>Web</span>
-            <span onClick={this.hanleMobileClick} className={this.state.webView ? null : 'active'}>Mobile</span>
-          </div> : <div className="anim-btn">
-              <span className="active">Web</span>
-            </div>}
-        </PreviewImages>
-        <div className="description" onTouchStart={this.handleDrag}>
-          <p>
-            {project.description}
-          </p>
         </div>
-        <div className="technologies">
-          {project.technologies.map((tech) => <span key={tech}>{tech}</span>)}
-        </div>
-        <div className="buttons">
-          <a href={project.repoLink} target="_blank">Source Code <FontAwesomeIcon icon={faGithub} /></a>
-          <a href={project.demoLink} target="_blank">Live Demo<FontAwesomeIcon icon={faExternalLinkAlt} /></a>
-        </div>
-      </SlideContainer>
-    )
-  }
+        {project.previews.length > 1 ? <div className="anim-btn">
+          <span onClick={hanleWebClick} className={isWebView ? 'active' : null}>Web</span>
+          <span onClick={hanleMobileClick} className={isWebView ? null : 'active'}>Mobile</span>
+        </div> : <div className="anim-btn">
+            <span className="active">Web</span>
+          </div>}
+      </PreviewImages>
+      <div className="description" onTouchStart={handleDrag}>
+        <p>
+          {project.description}
+        </p>
+      </div>
+      <div className="technologies">
+        {project.technologies.map((tech) => <span key={tech}>{tech}</span>)}
+      </div>
+      <div className="buttons">
+        <a href={project.repoLink} target="_blank">Source Code <FontAwesomeIcon icon={faGithub} /></a>
+        <a href={project.demoLink} target="_blank">Live Demo<FontAwesomeIcon icon={faExternalLinkAlt} /></a>
+      </div>
+    </SlideContainer>
+  )
 }
 
 export default ProjectSlide;
