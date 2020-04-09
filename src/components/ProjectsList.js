66c0a13ba5620f7c1de76e78f3b2d3c1a79113f1
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2020-04-09 12:51:39 +0100

    update projects

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -3,11 +3,31 @@ import ProjectSlide from './ProjectSlide';
 import mobileLinkOne from '../assets/img/1mobile.png';
 import webLinkOne from '../assets/img/1-web.png';
 import webLinkTwo from '../assets/img/2-web.png';
+import mobileLinkTwo from '../assets/img/2-mobile.png';
 import webLinkThree from '../assets/img/3-web.png';
+import mobileLinkThree from '../assets/img/3-mobile.png';
 import webLinkFour from '../assets/img/4-web.png';
 import mobileLinkFour from '../assets/img/4-mobile.png';
+import webLinkFive from '../assets/img/5-web.png';
+import mobileLinkFive from '../assets/img/5-mobile.png';
 
 const PROJECTS = [
+  {
+    title: 'Battleship Game',
+    description: 'Battleship (also Battleships or Sea Battle) is a strategy type guessing game for two players. We implemented a web version of the app where the two players are the user and his bot opponent.',
+    previews: [
+      webLinkThree,
+      mobileLinkThree
+    ],
+    technologies: [
+      'HTML',
+      'SCSS',
+      'JavaScript',
+      'Webpack'
+    ],
+    repoLink: 'https://github.com/stratospherique/battleship-js',
+    demoLink: 'https://stratospherique.github.io/battleship-js/'
+  },
   {
     title: 'REMA',
     description: 'REMA is a full stack web app that enables the user to navigate through a wide variety of Real Estates via nice and easy to use responsive interface.',
@@ -16,8 +36,8 @@ const PROJECTS = [
       mobileLinkOne,
     ],
     technologies: [
-      'html',
-      'css',
+      'HTML',
+      'SCSS',
       'JavaScript',
       'React',
       'Redux',
@@ -30,31 +50,17 @@ const PROJECTS = [
     title: 'ODIN Facebook',
     description: 'Odin Facebook is a mini blog that allows users to publish new posts about a variety of topics.',
     previews: [
-      webLinkTwo
+      webLinkTwo,
+      mobileLinkTwo
     ],
     technologies: [
-      'html',
-      'css',
+      'HTML',
+      'CSS',
       'Rails',
       'Ruby'
     ],
     repoLink: 'https://github.com/stratospherique/odin-facebook',
     demoLink: 'https://desolate-badlands-67931.herokuapp.com/'
-  },
-  {
-    title: 'Math Calculator',
-    description: 'Math calculator is a web app that enables the user to perform a set of basic Mathematical operations on Numbers and to evaluate Basic expressions',
-    previews: [
-      webLinkThree
-    ],
-    technologies: [
-      'html',
-      'SCSS',
-      'JavaScript',
-      'React'
-    ],
-    repoLink: 'https://github.com/stratospherique/Calculator-React/tree/master',
-    demoLink: 'https://gifted-bose-6eb952.netlify.com/'
   },
   {
     title: 'Weather-App-JS',
@@ -64,7 +70,7 @@ const PROJECTS = [
       mobileLinkFour,
     ],
     technologies: [
-      'html',
+      'HTML',
       'SCSS',
       'JavaScript',
       'Webpack',
@@ -73,7 +79,28 @@ const PROJECTS = [
     repoLink: 'https://github.com/stratospherique/Weather-App-JS/tree/development',
     demoLink: 'https://rawcdn.githack.com/stratospherique/Weather-App-JS/ef39618fc7a24fe22c79bb20a182d1814fc264c8/index.html'
   },
+
 ]
+
+/* to be added React Animations
+{
+    title: 'Dashboard',
+    description: 'Dashboard is a web app that mimics the behaviour and the look & feel of a dashboard along with validation-enabled forms.',
+    previews: [
+      webLinkFive,
+      mobileLinkFive
+    ],
+    technologies: [
+      'HTML',
+      'SCSS',
+      'JavaScript',
+      'React',
+      'Redux'
+    ],
+    repoLink: 'https://github.com/stratospherique/Dashboard',
+    demoLink: 'https://proto-dashboard.netlify.com/'
+  }
+*/
 
 const ProjectsList = ({ currentIndex }) => {
   return (
